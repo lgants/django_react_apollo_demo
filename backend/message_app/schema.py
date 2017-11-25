d63bd549ba6e8874cbad57c3cfe99ab1bd159452
lgants <lgants@gmail.com>
2017-11-25 13:48:37 -0600

    debugged tests

--- backend/message_app/schema.py
+++ backend/message_app/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-
+from graphql_relay.node.node import from_global_id
 from . import models
 
 
@@ -10,8 +10,16 @@ class MessageType(DjangoObjectType):
         interfaces = (graphene.Node, )
 
 
-class Query(graphene.AbstractType):
+class Query(graphene.ObjectType):
+    message = graphene.Field(MessageType, id=graphene.ID())
+
+    def resolve_message(self, args):
+        rid = from_global_id(args.get('id'))
+        # rid is a tuple: ('MessageType', '1')
+        return models.Message.objects.get(pk=rid[1])
+
+
     all_messages = graphene.List(MessageType)
 
-    def resolve_all_messages(self, args, context, info):
+    def resolve_all_messages(self, args):
         return models.Message.objects.all()
--- backend/message_app/tests/test_schema.py
+++ backend/message_app/tests/test_schema.py
@@ -2,6 +2,7 @@ import pytest
 from mixer.backend.django import mixer
 from .. import schema
 from IPython import embed
+from graphql_relay.node.node import to_global_id
 
 pytestmark = pytest.mark.django_db
 
@@ -10,9 +11,17 @@ def test_message_type():
     instance = schema.MessageType()
     assert instance
 
+def test_resolve_message():
+    # mixer.blend generates a model's instance and saves to db
+    msg = mixer.blend('message_app.Message')
+    q = schema.Query()
+    id = to_global_id('MessageType', msg.pk)
+    res = q.resolve_message({'id': id})
+    assert res == msg, 'Should return the requested message'
+
 def test_resolve_all_messages():
     mixer.blend('message_app.Message')
     mixer.blend('message_app.Message')
     q = schema.Query()
-    res = q.resolve_all_messages(None, None, None)
+    res = q.resolve_all_messages(None)
     assert res.count() == 2, 'Should return all messages'
